541cf2d770ad9b7b361aadbe435a9093e663f721
Jamelly <Jamelly@Lenovo-PC>
2019-11-21 23:50:35 -0200

    Combobox zonas

--- ECommerceViewController/src/controller/CadastroController.java
+++ ECommerceViewController/src/controller/CadastroController.java
@@ -5,9 +5,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import telas.TelaCadastro;
@@ -17,12 +20,19 @@ public class CadastroController implements Initializable {
     
     @FXML private Button btnCadastro;
     @FXML private Button btnVoltar;
-
+    @FXML private ComboBox<String> cbZona;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+       
+        
+        
         btnCadastro.setOnMouseClicked((MouseEvent e)->{
-     
+            ObservableList<String> zonas = FXCollections.observableArrayList("Norte","Sul","Leste","Oeste");
+            cbZona.setValue("Zonas");
+            cbZona.setItems(zonas);
+            cbZona.getSelectionModel().getSelectedItem();// select item escolhido no comboBox;
     }); 
         
         btnVoltar.setOnMouseClicked((MouseEvent e)->{
